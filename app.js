816cbe64511555a80383b9c63609f43806caaa21
kappyllo <33810546+kappyllo@users.noreply.github.com>
2022-05-18 09:55:46 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const modalBg = document.querySelector('.modal-bg');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelector('.getstartedbtn');
+const btnOpenNav = document.querySelector('.hamburger');
+const btnCloseNav = document.querySelector('.closeBtn');
+const navOnMobile = document.querySelector('.navbar');
 
 btnOpenModal.addEventListener('click', function () {
   modalBg.classList.remove('hidden');
@@ -10,4 +13,12 @@ btnOpenModal.addEventListener('click', function () {
 
 btnCloseModal.addEventListener('click', function () {
   modalBg.classList.add('hidden');
+});
+
+btnOpenNav.addEventListener('click', function () {
+  navOnMobile.style.display = 'flex';
+});
+
+btnCloseNav.addEventListener('click', function () {
+  navOnMobile.style.display = 'none';
 });
